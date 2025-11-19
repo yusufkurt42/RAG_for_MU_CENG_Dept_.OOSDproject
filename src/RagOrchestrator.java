95f9e3deaea679cf102d0de146b66c8fe0e5b5a9
yusufkurt42 <ykurt2004@gmail.com>
2025-11-20 01:17:32 +0300

    Getting things on hand together

---
+++ src/RagOrchestrator.java
@@ -0,0 +1,10 @@
+public class RagOrchestrator {
+    public static void main(String[] args){
+        /*
+        Example input:
+        java -jar rag.jar --config config.-yaml --q
+        "Murat Can Ganiz’in ofis numarasi nedir?"
+        */
+        System.out.println("test");
+    }
+}
