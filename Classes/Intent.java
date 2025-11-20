7f43aa1402c50619dc649203ecf39b149f6bf991
Yusuf Kurt <143208450+yusufkurt42@users.noreply.github.com>
2025-11-20 17:15:52 +0300

    Delete Classes directory

--- Classes/Intent.java
+++
@@ -1,9 +0,0 @@
-package OOPCode;
-
-public enum Intent {
-    STAFF_LOOKUP, // ordinal = 0
-    REGISTRATION, // ordinal = 1
-    POLICY_FAQ,   // ordinal = 2
-    COURSE,       // ordinal = 3
-    UNKNOWN       // ordinal = 4
-}
